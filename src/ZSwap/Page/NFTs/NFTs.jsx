043353b825bd56be598bd5d01ab765a73346a9f7
tranhuyducseven <tranhuyducseven@users.noreply.github.com>
2023-09-04 16:24:54 +0700

    feature(nfts): add Burn button

--- src/ZSwap/Page/NFTs/NFTs.jsx
+++ src/ZSwap/Page/NFTs/NFTs.jsx
@@ -22,40 +22,48 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   padding: 20px;
+`;
+
+const Card = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  border-radius: 50px;
+  overflow: hidden;
   background-color: rgba(0, 0, 0, 0.05);
   border-radius: 10px;
   border: 2px solid #ddd;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
 
-const Table = styled.div`
-  width: 100%;
-  padding: 20px;
-  table {
-    table-layout: fixed;
-    width: 100%;
-    border-collapse: collapse;
-    border-spacing: 0;
-    font-size: 18px;
-    thead {
-      tr {
-        th {
-          padding: 24px 16px;
-          text-align: left;
-          border-bottom: 2px solid #2bcc91;
-          background-color: rgba(13, 148, 136, 0.05);
-          border-radius: 10px 10px 0 0;
-        }
-      }
-    }
-  }
-`;
-
-const Tr = styled.tr`
+const Button = styled.button`
+  height: 50px;
+  width: ${(props) => props.width || "100%"};
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props.background ?? "#2BCC91"};
+  color: white;
+  border-radius: 10px;
+  font-weight: bold;
   overflow: hidden;
-  td {
-    padding: 24px 16px;
-    text-align: left;
-    background-color: ${(props) => props.cellColor || "white"};
+  padding: ${padding === "normal" ? "8px 0" : "12px 24px"};
+  cursor: pointer;
+  border: none;
+  z-index: 0;
+  &:disabled {
+    background: #9ca3af;
+    color: white;
+  }
+
+  &:hover {
+    opacity: ${props.disabled ? "1" : "0.8"};
+  }
+  &:hover:before {
+    opacity: ${props.disabled ? "0" : "1"};
   }
 `;
 
@@ -105,22 +113,22 @@ return (
     <Container>
       {state.NFTs?.map((token, index) => (
         <div
-          className={index}
           style={{
-            width: "33.33333%",
-            padding: "10px",
-            borderRadius: "50px",
-            overflow: "hidden",
+            padding: "20px 10px",
+            width: "33.333333%",
           }}
         >
-          <img
-            src={token.image}
-            alt={`NFT of ${token.title}`}
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          />
+          <Card key={index}>
+            <img
+              src={token.image}
+              alt={`NFT of ${token.title}`}
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            />
+            <Button>Burn</Button>
+          </Card>
         </div>
       ))}
     </Container>
