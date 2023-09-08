0474d1b13e8fe6ffad578ded9aadae619442e44a
Galin Chung Nguyen <galin-chung-nguyen@gmail.com>
2023-09-08 08:33:03 +0700

    feat: create pool and view list of tokens

--- src/ZSwap/Page/Pools/Pools.jsx
+++ src/ZSwap/Page/Pools/Pools.jsx
@@ -14,7 +14,12 @@ const MAX_TICK = 887272;
 
 const MIN_PRICE = Math.pow(1.0001, MIN_TICK);
 const MAX_PRICE = Math.pow(1.0001, MAX_TICK);
-
+const TOKEN_SUFFIX = ".zswap.testnet";
+const ZSWAP_MANAGER = "manager3.zswap.testnet";
+const ZSWAP_FACTORY = "factory3.zswap.testnet";
+const DEFAULT_SQRT_PRICE_X96 = "792281625142643375935439503360";
+const DEFAULT_LOGO =
+  "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 /** State */
 State.init({
   firstSelectedToken: {
@@ -32,6 +37,10 @@ State.init({
   priceRange: {
     lowPrice: "",
     highPrice: "",
+  },
+  customTokenInput: {
+    firstTokenAddress: "",
+    secondTokenAddress: "",
   },
   TOKENS: {},
   fetchedTokensList: false,
@@ -77,7 +86,7 @@ function initFetchListOfSampleTokens() {
 const TOKEN_ACCOUNTS = ["zusd.zswap.testnet", "znear.zswap.testnet"];
 const poolMetadata =
   state.firstSelectedToken.symbol && state.secondSelectedToken.symbol
-    ? Near.view("factory2.zswap.testnet", "get_pool", {
+    ? Near.view(ZSWAP_FACTORY, "get_pool", {
         token_0: state.TOKENS[state.firstSelectedToken.symbol]?.address ?? "",
         token_1: state.TOKENS[state.secondSelectedToken.symbol]?.address ?? "",
         fee: fee_descriptions[state.fee_chosen][0] * Math.pow(10, 4),
@@ -143,6 +152,25 @@ function $fetchTokenMetadata() {
   });
 }
 
+const refetchTokenMetadata = (address) => {
+  return Near.asyncView(address, "ft_metadata", {}).then((tokenMetadata) => {
+    State.update({
+      TOKENS: {
+        ...state.TOKENS,
+        [tokenMetadata.symbol]: {
+          ...tokenMetadata,
+          address: tokenAddress,
+          priceInUSD: 0,
+          address: address,
+          priceInUSD: -1,
+          icon: tokenMetadata?.icon ?? DEFAULT_LOGO,
+          logo: tokenMetadata?.icon ?? DEFAULT_LOGO,
+        },
+      },
+    });
+    return tokenMetadata.symbol;
+  });
+};
 if (!state.fetchedTokensList) {
   State.update({
     fetchedTokensList: true,
@@ -166,7 +194,8 @@ const Main = styled.div`
   padding: 40px;
   border-radius: 10px;
   //shadow-xl
-  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1),
+  box-shadow:
+    0 20px 25px -5px rgb(0 0 0 / 0.1),
     0 8px 10px -6px rgb(0 0 0 / 0.1);
   color: black;
 `;
@@ -237,6 +266,7 @@ const connectWallet =
 const getPriceOfTokenIfNeeded = (token) => {
   if (!token) return;
   if (state.TOKENS[token].priceInUSD < 0) {
+    return 0;
     console.log("Start fetching price for " + token + "...");
     asyncFetch(
       "https://api.unmarshal.com/v1/pricestore/" +
@@ -305,11 +335,12 @@ const getTokenBalanceOfUser = (token) => {
     account_id: accountId,
   })
     .catch((err) => 0)
-    .then((bl) =>
-      Big(bl)
+    .then((bl) => {
+      console.log("User has ", bl, " ", token);
+      return Big(bl)
         .div(Big(Math.pow(10, 24)))
-        .toNumber()
-    )
+        .toNumber();
+    })
     .then((bl) => (Number.isNaN(bl) ? 0 : bl));
 };
 
@@ -320,7 +351,7 @@ const updateSelectedData = () => {
     State.update({
       firstSelectedToken: {
         ...state.firstSelectedToken,
-        balance,
+        balance: balance ?? 0,
       },
     });
   });
@@ -328,7 +359,7 @@ const updateSelectedData = () => {
     State.update({
       secondSelectedToken: {
         ...state.secondSelectedToken,
-        balance,
+        balance: balance ?? 0,
       },
     });
   });
@@ -759,6 +790,9 @@ const updateAmount1BasedOnAmount0 = () => {
     });
     return;
   }
+
+  if (!state.priceRange.lowPrice.length || !state.priceRange.highPrice.length)
+    return;
   const isReversedDirection =
     poolMetadata &&
     !poolMetadata.token_0.startsWith(
@@ -777,7 +811,7 @@ const updateAmount1BasedOnAmount0 = () => {
 
   if (poolMetadata)
     Near.asyncView(
-      "manager.zswap.testnet",
+      ZSWAP_MANAGER,
       isReversedDirection
         ? "calculate_amount_0_with_amount_1"
         : "calculate_amount_1_with_amount_0",
@@ -816,6 +850,8 @@ const updateAmount0BasedOnAmount1 = () => {
     return;
   }
 
+  if (!state.priceRange.lowPrice.length || !state.priceRange.highPrice.length)
+    return;
   const isReversedDirection =
     poolMetadata &&
     !poolMetadata.token_0.startsWith(
@@ -834,7 +870,7 @@ const updateAmount0BasedOnAmount1 = () => {
 
   if (poolMetadata)
     Near.asyncView(
-      "manager.zswap.testnet",
+      ZSWAP_MANAGER,
       isReversedDirection
         ? "calculate_amount_1_with_amount_0"
         : "calculate_amount_0_with_amount_1",
@@ -905,6 +941,10 @@ const canProvideLiquidity =
     Number(state.secondSelectedToken.amount) > 0) &&
   !notEnoughAmount0 &&
   !notEnoughAmount1;
+const canCreatePool =
+  !poolMetadata &&
+  state.firstSelectedToken.symbol &&
+  state.secondSelectedToken.symbol;
 
 const canDeposit =
   (Number(state.firstSelectedToken.amount) ||
@@ -1593,7 +1633,29 @@ const ErrorBox = (
   </ErrorBoxWrapper>
 );
 
-const createPool = () => {};
+const createPool = () => {
+  let token_0 = state.firstSelectedToken.symbol;
+  let token_1 = state.secondSelectedToken.symbol;
+
+  if (token_0 > token_1) {
+    let tmp = token_0;
+    token_0 = token_1;
+    token_1 = tmp;
+  }
+
+  Near.call(
+    ZSWAP_MANAGER,
+    "create_pool",
+    {
+      token_0: state.TOKENS[token_0]?.address ?? token_0 + TOKEN_SUFFIX,
+      token_1: state.TOKENS[token_1]?.address ?? token_1 + TOKEN_SUFFIX,
+      fee: fee_descriptions[state.fee_chosen][0] * Math.pow(10, 4),
+      sqrt_price_x96: DEFAULT_SQRT_PRICE_X96,
+    },
+    300000000000000,
+    Math.pow(10, 23)
+  );
+};
 const provideLiquidity = () => {
   const isReversedDirection =
     poolMetadata &&
@@ -1612,7 +1674,7 @@ const provideLiquidity = () => {
     : getUpperTick(state.priceRange.highPrice).toNumber();
 
   Near.call(
-    "manager.zswap.testnet",
+    ZSWAP_MANAGER,
     "mint",
     {
       params: {
@@ -1653,20 +1715,24 @@ const ProvideLiquidityButton = (
     src={`${config.ownerId}/widget/ZSwap.Element.Button`}
     props={{
       onClick: () => {
-        if (provideLiquidityOnExistingPool) {
+        if (canProvideLiquidity && provideLiquidityOnExistingPool) {
           provideLiquidity();
-        } else {
+        } else if (canCreatePool && !provideLiquidityOnExistingPool) {
           createPool();
         }
       },
       text: provideLiquidityOnExistingPool
         ? "Provide liquidity"
-        : "Create pool and provide liquidity",
+        : "Create pool",
       styles: {
         width: "100%",
         color: "white",
       },
-      background: canProvideLiquidity ? undefined : "#8bf1cc",
+      background:
+        canProvideLiquidity ||
+        (canCreatePool && !provideLiquidityOnExistingPool)
+          ? undefined
+          : "#8bf1cc",
     }}
   />
 );
@@ -1729,6 +1795,13 @@ const PoolDiaglog = (
                 onChangeItem: onFirstTokenChange,
                 background: "rgb(245, 246, 252)",
                 fontSize: "16px",
+                // custom input
+                enableCustomInput: true,
+                onSubmitCustomInputValue: (address) => {
+                  refetchTokenMetadata(address).then((symbol) => {
+                    onFirstTokenChange(symbol);
+                  });
+                },
               }}
               style={{
                 flex: 1,
@@ -1753,6 +1826,13 @@ const PoolDiaglog = (
                 onChangeItem: onSecondTokenChange,
                 background: "rgb(245, 246, 252)",
                 fontSize: "16px",
+                // custom input
+                enableCustomInput: true,
+                onSubmitCustomInputValue: (address) => {
+                  refetchTokenMetadata(address).then((symbol) => {
+                    onSecondTokenChange(symbol);
+                  });
+                },
               }}
               style={{
                 flex: 1,
